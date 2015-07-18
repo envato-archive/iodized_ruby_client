97a168c7d670f2209a00217720d1457787348093
Tu Hoang <rebyn@me.com>
2015-07-19 09:27:27 +1000

    Iodized.do? was dropped in favor of Iodized.on?
    Ref: 207a2946

--- example/sinatra_iodized.rb
+++ example/sinatra_iodized.rb
@@ -6,7 +6,7 @@ Iodized.client = Iodized::Client.new('0.0.0.0', 5353)
 use Iodized::Middleware
 
 get '/cool_story' do
-  @joe = Iodized.do?(:cool_story_joe, params)
+  @joe = Iodized.on?(:cool_story_joe, params)
   @features = Thread.current[:iodized_feature_set]
   erb :cool_story
 end
